fcdce6b5d1d6ab99d1d2f0d5aa55804eb755940d
maudnals <maud.nalpas@gmail.com>
2022-07-04 17:23:47 +0200

    Feat: Added support for local headers

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,19 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    // https://github.com/vitejs/vite/issues/3909#issuecomment-934044912
+    {
+      name: 'configure-response-headers',
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Referrer-Policy', 'no-referrer'), next();
+        });
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
