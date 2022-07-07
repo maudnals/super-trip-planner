7ed9459c0707d754387fa72934f05581c3bf89f9
maudnals <maud.nalpas@gmail.com>
2022-07-07 17:15:10 +0200

    Feat: styled navigation

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,9 @@ import HelloWorld from '@/components/HelloWorld.vue';
     </div>
   </header>
 
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style>
@@ -32,56 +34,62 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 #app {
   max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
   font-weight: normal;
 }
 
 .app-name {
   font-family: TheWantersDEMO;
   font-size: 2rem;
-  color: #aee5eb;
   text-align: center;
-  text-shadow: -1px 2px 0 #ff3f00;
+  color: var(--color-indigo-x-light);
+  text-shadow: -1px 2px 0 var(--color-orange);
 }
 
 header {
   line-height: 1.5;
   max-height: 100vh;
+  background: #005258;
+  width: 100vw;
+  padding: 0.5rem;
+  box-shadow: var(--color-indigo-transparent) 0px 6px 20px 0px;
+}
+
+main {
+  padding: 2rem;
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto;
 }
 
-a,
-.green {
+a {
   text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
 }
 
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+nav a {
+  text-decoration: none;
+  color: var(--color-indigo-x-light);
+  transition: 0.3s;
+}
+
+nav a:hover {
+  background-color: var(--color-indigo-x-light-transparent);
 }
 
 nav {
   width: 100%;
-  font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
+  padding: 1rem 0;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  background-color: var(--color-indigo-x-light-transparent);
+  color: var(--color-text-inverted);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  background-color: var(--color-indigo-x-light-transparent);
 }
 
 nav a {
@@ -92,43 +100,5 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
