155163b15d43ae9a89d445a6c5fb870ab6489024
maudnals <maud.nalpas@gmail.com>
2022-07-07 16:51:00 +0200

    Feat: created trip inf point component

---
+++ src/components/TripInfoPoint.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  info: string;
+}>();
+</script>
+
+<template>
+  <div class="wrapper">
+    <label>{{ label }}</label>
+    <div>{{ info }}</div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: flex-end;
+  border-top: 0.7px solid var(--color-border);
+  padding: 0.4rem 0.2rem;
+}
+
+label {
+  width: 16rem;
+  font-family: RobotoMono-Regular;
+  color: var(--color-label);
+  letter-spacing: 2px;
+  font-size: 0.8rem;
+  text-transform: uppercase;
+}
+</style>
