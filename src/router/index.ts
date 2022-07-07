4c13b6d7c7b8bbab5291f0e84a7554d7a40f9d64
maudnals <maud.nalpas@gmail.com>
2022-07-07 17:20:55 +0200

    Feat: prepared needed routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import TripsListView from '../views/TripsListView.vue';
+import NewTripView from '../views/NewTripView.vue';
+import TripView from '../views/TripView.vue';
+import NotFoundView from '../views/NotFoundView.vue';
+import TripIdeasView from '../views/TripIdeasView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +21,31 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/trip-ideas',
+      name: 'trip ideas',
+      component: TripIdeasView,
+    },
+    {
+      path: '/trips',
+      name: 'trips',
+      component: TripsListView,
+    },
+    {
+      path: '/new-trip',
+      name: 'new trip',
+      component: NewTripView,
+    },
+    {
+      path: '/trip',
+      name: 'trip',
+      component: TripView,
+    },
+    {
+      path: '/not-found',
+      name: 'not found',
+      component: NotFoundView,
     },
   ],
 });
