97e80bdbba18205561b4f2932a78dd42c4ca4694
maudnals <maud.nalpas@gmail.com>
2022-07-07 16:53:31 +0200

    Feat: created trip card component for trip list view

---
+++ src/components/TripCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  endDate: String;
+  location: String;
+  startDate: String;
+  title: String;
+}>();
+</script>
+
+<template>
+  <div class="wrapper">
+    <h2 class="title">{{ title }}</h2>
+    <div>{{ location }}</div>
+    <div>{{ startDate }} ➣ {{ endDate }}</div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  background: var(--color-card);
+  margin: 1rem 0;
+  padding: 1rem;
+  border-radius: 8px;
+  color: var(--color-text);
+  box-shadow: var(--color-shadow-light) 0px 7px 29px 0px;
+  transition: box-shadow 0.2s linear, transform 0.1s linear;
+}
+
+.wrapper:hover {
+  box-shadow: var(--color-shadow-dark) 0px 7px 29px 0px;
+  transform: translateY(-0.3rem);
+}
+</style>
