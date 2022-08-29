3b69917de1ce0e0c88567b232f97e6253fef4d4b
maudnals <maud.nalpas@gmail.com>
2022-08-29 15:29:15 +0200

    Feat: Added debugger view

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,30 +4,14 @@ import NewTripView from '../views/NewTripView.vue';
 import TripView from '../views/TripView.vue';
 import NotFoundView from '../views/NotFoundView.vue';
 import TripIdeasView from '../views/TripIdeasView.vue';
+import DebuggerView from '../views/DebuggerView.vue';
+import AboutView from '../views/AboutView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/trip-ideas',
-      name: 'trip ideas',
-      component: TripIdeasView,
-    },
-    {
-      path: '/trips',
       name: 'trips',
       component: TripsListView,
     },
@@ -40,6 +24,21 @@ const router = createRouter({
       path: '/trip',
       name: 'trip',
       component: TripView,
+    },
+    {
+      path: '/trip-ideas',
+      name: 'trip ideas',
+      component: TripIdeasView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+    },
+    {
+      path: '/debugger',
+      name: 'debugger',
+      component: DebuggerView,
     },
     {
       path: '/not-found',
---
+++ src/views/DebuggerView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      platform: navigator?.userAgentData?.platform,
+      isMobile: navigator?.userAgentData?.mobile,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>Platform: {{ platform }}</div>
+  <div>isMobile: {{ isMobile }}</div>
+</template>
