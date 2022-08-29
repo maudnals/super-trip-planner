d88c4b141d95e1e45ef2a51ca6e73172d1e3be0e
maudnals <maud.nalpas@gmail.com>
2022-08-29 15:27:03 +0200

    Refactor: removed unused styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 import TripsListView from '../views/TripsListView.vue';
 import NewTripView from '../views/NewTripView.vue';
 import TripView from '../views/TripView.vue';
