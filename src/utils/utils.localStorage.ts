ce391e70606c50a9c336186cdb19b7021fa0b4fe
maudnals <maud.nalpas@gmail.com>
2022-09-02 10:49:12 +0200

    Refactor: Moved localStorage functs to utils

--- src/utils/utils.localStorage.ts
+++ src/utils/utils.localStorage.ts
@@ -2,3 +2,7 @@ export function getTrips() {
   const trips = localStorage.getItem('trips');
   return trips ? JSON.parse(trips) : {};
 }
+
+export function setTrips(trips: string) {
+  localStorage.setItem('trips', trips);
+}
